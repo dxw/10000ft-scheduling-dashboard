50f68d82622f2992d253fb4d3807c8660281f502
Tom Hipkin <tomhipkin@gmail.com>
2017-12-12 15:44:39 +0000

    (feature): Thumbnails!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < SimpleDelegator
+  def first_name
+    self['first_name']
+  end
+
   def display_name
     self['display_name']
+  end
+
+  def thumbnail
+    self['thumbnail'].present? ? self['thumbnail'] : default_thumbnail
+  end
+
+  private def default_thumbnail
+    'wall.jpg'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe User, type: :model do
       expect(result.display_name).to eq('Poss Apostolou')
     end
   end
+
+  describe '#thumbail' do
+    it 'returns the external display_name' do
+      result = User.new('thumbnail' => 'https://d1zljzhvpuyaiz.cloudfront.net/xxx.jpg')
+      expect(result.thumbnail).to eq('https://d1zljzhvpuyaiz.cloudfront.net/xxx.jpg')
+    end
+
+    context 'when there is no thumbnail' do
+      it 'return the default wall' do
+        result = User.new('thumbnail' => '')
+        expect(result.thumbnail).to eq('wall.jpg')
+      end
+    end
+  end
 end
