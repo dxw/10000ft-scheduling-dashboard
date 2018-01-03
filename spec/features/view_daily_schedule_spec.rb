633c096e11bb6c258aef61edd167b63b4f3567ef
Tom Hipkin <tomhipkin@gmail.com>
2018-01-03 11:32:46 +0000

    (spec): Complete test suite by asserting sickness displays correctly

--- spec/features/view_daily_schedule_spec.rb
+++ spec/features/view_daily_schedule_spec.rb
@@ -76,6 +76,18 @@ RSpec.feature 'View the daily schedule' do
   end
 
   scenario 'users are sick' do
+    allow(client).to receive(:get_leave_types)
+      .and_return(TenThousandFeetStubs.leave_types_response(project_id: 123, name: 'Sick'))
+    allow(client).to receive(:get_assignments)
+      .and_return(TenThousandFeetStubs.assignment_response(project_id: 123, user_id: 456))
+    allow(client).to receive(:get_users)
+      .and_return(TenThousandFeetStubs.user_response(user_id: 456, first_name: 'first-name'))
 
+    visit root_path
+
+    within('.sick') do
+      expect(page).to have_content('Sick')
+      expect(page).to have_content('first-name')
+    end
   end
 end
