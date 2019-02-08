c17c9ee74740061b2921445c079753d0ea15c485
Cristina <cristina@dxw.com>
2019-02-08 18:11:34 +0000

    (chore) Remove old 10kft client

--- lib/ten_thousand_feet_wrapper.rb
+++
@@ -1,6 +0,0 @@
-module TenThousandFeetWrapper
-  def self.client
-    raise ArgumentError, 'ENV is missing an API key: `SCHEDULING_API_KEY`' unless ENV['SCHEDULING_API_KEY'].present?
-    TenThousandFeet.new(auth: ENV['SCHEDULING_API_KEY'])
-  end
-end
