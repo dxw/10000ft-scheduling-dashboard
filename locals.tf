6bfb2db17ef3b551396bda966aa5c342fd3d6721
Chris Wright <chris@dxw.com>
2019-09-06 10:38:16 +0100

    (Feature) dalmatian config
    * Adds the fiels required to launch the app onto dalmatian

---
+++ locals.tf
@@ -0,0 +1,10 @@
+locals {
+  app_name = "10kft"
+
+  app_github_owner = "dxw"
+  app_github_repo  = "10000ft-scheduling-dashboard"
+  app_service_name = "${local.app_name}-${var.environment}"
+
+  app_container_entrypoint = ["./docker-entrypoint.sh"]
+  app_container_command    = ["bundle", "exec", "puma", "-p", "5000"]
+}
